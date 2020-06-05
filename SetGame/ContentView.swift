b0f65a70f758c7cb3e591fc6c6fe0011d8f50a66
Igor Malyarov <igor1309@gmail.com>
2020-06-05 23:58:46 +0300

    Initial Commit

---
+++ SetGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SetGame
+//
+//  Created by Igor Malyarov on 05.06.2020.
+//  Copyright © 2020 Igor Malyarov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
