567030fc1b897b029e54638884b853fbbdc8b71e
Igor Malyarov <igor1309@gmail.com>
2022-11-28 13:36:50 +0300

    add `AppStorePreviewsTests` target

---
+++ SetGame/AppStorePreviewsTests/AppStorePreviewsTests.swift
@@ -0,0 +1,13 @@
+//
+//  AppStorePreviewsTests.swift
+//  AppStorePreviewsTests
+//
+//  Created by Igor Malyarov on 28.11.2022.
+//  Copyright © 2022 Igor Malyarov. All rights reserved.
+//
+
+import XCTest
+
+final class AppStorePreviewsTests: XCTestCase {
+
+}
